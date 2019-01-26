f0f5353f44a8604b9210d20a2bee172793e02250
Pete Kreppein <pmkreppein@gmail.com>
2019-01-26 15:34:36 -0500

    Clean up project to start actual Photowall app

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,20 +1,13 @@
 import React, {Component} from 'react';
-import List from './List';
 import Title from './Title';
 
-const tasksArray = ['Vue', 'Phoenix/Elixir','Unity', 'AR Kit']
 
 class Main extends Component {
   render(){
     return (
       <div>
-        <Title title={"Rendered from array in JS File:"}/>
-        <List tasks={tasksArray}/>
-        <Title title={"Showing component using on the fly array for props:"}/>
-        <List tasks={["Random","Array","For","Task","List"]}/>
+        <Title title={"Hello World!"}/>
 
-        <Title title={"After shoving components into their own files:"}/>
-        <List tasks={["Figure","Out","If","There","is","a","React","CLI"]}/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,37 +1,4 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main';
-const tasksArray = ['Vue', 'Phoenix/Elixir','Unity', 'AR Kit']
-const element = React.createElement('h1', null, "Hello World!");
-
-const orderedList = React.createElement('ol', null,
-  React.createElement('li', null, "Learn React."),
-  React.createElement('li', null, "Learn Vue."),
-  React.createElement('li', null, "Learn Phoenix."),
-  React.createElement('li', null, "Learn Unity or Unreal."),
-);
-
-const mappedOrderedList = React.createElement('ol', null,
-  tasksArray.map((task, index) => React.createElement('li', {key: index}, task))
-);
-
-const jsxElement =
-<div>
-  <h1>Task List: </h1>
-  <ol>
-    {tasksArray.map((task, index) => <li key = {index}> {task} </li>)}
-  </ol>
-</div>
-
-
-
-
-
-
-
-
-ReactDOM.render(element, document.getElementById('root'));
-ReactDOM.render(orderedList, document.getElementById('ordered-list'));
-ReactDOM.render(mappedOrderedList, document.getElementById('mapped-ordered-list'));
-ReactDOM.render(jsxElement, document.getElementById('jsx-element'));
-ReactDOM.render(<Main />, document.getElementById('class-list'));
+ReactDOM.render(<Main />, document.getElementById('root'));
