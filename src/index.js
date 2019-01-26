8e0cd463048721bc254b3b58d443d168ef70d271
Pete Kreppein <pmkreppein@gmail.com>
2019-01-26 13:14:38 -0500

    Initial commit and components video

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const tasks = ['Vue', 'Phoenix/Elixir','Unity', 'AR Kit']
+const element = React.createElement('h1', null, "Hello World!");
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const orderedList = React.createElement('ol', null,
+  React.createElement('li', null, "Learn React."),
+  React.createElement('li', null, "Learn Vue."),
+  React.createElement('li', null, "Learn Phoenix."),
+  React.createElement('li', null, "Learn Unity or Unreal."),
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const mappedOrderedList = React.createElement('ol', null,
+  tasks.map((task, index) => React.createElement('li', {key: index}, task))
+);
+
+ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(orderedList, document.getElementById('ordered-list'));
+ReactDOM.render(mappedOrderedList, document.getElementById('mapped-ordered-list'));
