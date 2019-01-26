143ef631a15e33c33169579ed3ffdf7d7b536066
Pete Kreppein <pmkreppein@gmail.com>
2019-01-26 16:34:11 -0500

    Class components working

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class Title extends Component {
     render() {
-      return <h2> {this.props.title} </h2>
+      return <h1> {this.props.title} </h1>
     }
 }
 export default Title;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main';
+import './styles/stylesheet.css';
 ReactDOM.render(<Main />, document.getElementById('root'));
