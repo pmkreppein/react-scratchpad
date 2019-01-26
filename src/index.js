991d89596274ee1c9091325d47f92fc58616f0e9
Pete Kreppein <pmkreppein@gmail.com>
2019-01-26 14:02:38 -0500

    Using props and Class based components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-const tasks = ['Vue', 'Phoenix/Elixir','Unity', 'AR Kit']
+const tasksArray = ['Vue', 'Phoenix/Elixir','Unity', 'AR Kit']
 const element = React.createElement('h1', null, "Hello World!");
 
 const orderedList = React.createElement('ol', null,
@@ -11,19 +11,50 @@ const orderedList = React.createElement('ol', null,
 );
 
 const mappedOrderedList = React.createElement('ol', null,
-  tasks.map((task, index) => React.createElement('li', {key: index}, task))
+  tasksArray.map((task, index) => React.createElement('li', {key: index}, task))
 );
 
 const jsxElement =
 <div>
   <h1>Task List: </h1>
   <ol>
-    {tasks.map((task, index) => <li key = {index}> {task} </li>)}
+    {tasksArray.map((task, index) => <li key = {index}> {task} </li>)}
   </ol>
 </div>
 
+
+class List extends Component {
+  render(){
+    return(<div>
+
+      <ol>
+        {this.props.tasks.map((task, index) => <li key = {index}> {task} </li>)}
+      </ol>
+    </div>)
+  }
+}
+
+class  Title extends Component {
+    render() {
+      return <h2> {this.props.title} </h2>
+    }
+}
+
+class Main extends Component {
+  render(){
+    return (
+      <div>
+        <Title title={"Rendered from array in JS File:"}/>
+        <List tasks={tasksArray}/>
+        <Title title={"Showing component using on the fly array for props:"}/>
+        <List tasks={["Random","Array","For","Task","List"]}/>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(element, document.getElementById('root'));
 ReactDOM.render(orderedList, document.getElementById('ordered-list'));
 ReactDOM.render(mappedOrderedList, document.getElementById('mapped-ordered-list'));
 ReactDOM.render(jsxElement, document.getElementById('jsx-element'));
+ReactDOM.render(<Main />, document.getElementById('class-list'));
