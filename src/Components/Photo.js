aa858d51981947e68099c04b7b27a69f3b28eb99
Pete Kreppein <pmkreppein@gmail.com>
2019-01-26 23:07:48 -0500

    Delete button working

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 
 function Photo(props){
   const post = props.post;
-  return( <figure className="figure">
+  return(<figure className="figure">
             <img className='photo' src={post.imageLink} alt={post.description}/>
             <figcaption> <p> {post.description} </p> </figcaption>
             <div className='button-container'>
-              <button className='remove-button'>Remove</button>
+              <button className='remove-button' onClick={() => {props.onRemovePhoto(post)}}>Remove</button>
             </div>
           </figure>);
 
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -4,9 +4,13 @@ import Photo from './Photo'
 
 function PhotoWall(props){
   return(
-   <div className="photoGrid">
-    {props.posts.map((post, index) => <Photo key={index} post={post}/>)}
-   </div>)
+    <div>
+    <button className='addIcon'></button> 
+    <div className="photoGrid">
+    {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+   </div>
+   </div>
+ )
 }
 
 // Stateless Component prior to making it Stateless
